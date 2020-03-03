6c468c37bab7b2937f4705299b398bc6aac9d407
Gibson <IanGibson@clearchannel.com>
2020-03-03 15:26:04 -0600

    continue work tonight

---
+++ app.js
@@ -0,0 +1,91 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose")
+
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/todolistDB", { useNewUrlParser: true, useUnifiedTopology: true } )
+
+const itemSchema = {
+  name: String
+}
+
+const Item = mongoose.model("Item", itemSchema)
+
+const item1 = new Item({
+  name: "example 1 ..."
+})
+
+const item2 = new Item({
+  name: "example 2 ..."
+})
+
+const item3 = new Item({
+  name: "example 3 ..."
+})
+
+
+app.get("/", function(req, res) {
+  const defaultItems = [item1, item2, item3]
+
+    Item.find({}, function(err, foundItems){
+      
+        if (foundItems.length === 0) {
+            Item.insertMany(defaultItems, function(err){
+        if (err) {
+            console.log(err)
+        } else {
+            console.log("successfully added items to database.")
+        }
+    })
+        res.redirect("/")
+      } else {
+        res.render("list", {listTitle: "Today", newListItems: foundItems});
+      }   
+    })
+  });
+
+app.post("/", function(req, res){
+
+  const itemName = req.body.newItem;
+
+  const item = new Item({
+    name: itemName
+  })
+
+  item.save()
+  res.redirect("/")
+
+});
+
+app.post("/delete", function(req, res){
+
+  const checkedItemId = req.body.checkbox
+  
+  Item.findByIdAndRemove(checkedItemId, function(err){
+    if (!err) {
+      console.log("Successfully deleted checked item.")
+      res.redirect("/")
+    }
+  })
+})
+
+app.get("/work", function(req,res){
+  res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.get("/about", function(req, res){
+  res.render("about");
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
